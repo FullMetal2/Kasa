5a092cd87ed09d4008140be871d2616d0d895e55
Jb <jean.baptiste.bertron@gmail.com>
2025-07-17 18:50:49 +0200

    composant banner ajouter a home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Home.scss';
-import Banner from './components/Banner/Banner.jsx';
+import Banner from '../../components/Banner/Banner';
 
 
-function Home ()  {
+function Home () {
   return (
     <div className='home-container'>
       <Banner />
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,13 +3,14 @@ import './Banner.scss';
 import bannerImage from '../../assets/images/Image_source_1.png';
 
 function Banner () {
-
+    return (
       <section className='container-banner'>
         <div className='banner'
         style={{backgroundImage: `url(${bannerImage})`}}>
           <h1>Chez vous, partout et ailleurs</h1>
         </div>
       </section>
+    )
 }
 
 export default Banner;
