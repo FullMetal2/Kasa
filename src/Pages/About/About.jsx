cccd54153fb4d8d10175f36f008dee6169423e63
Jb <jean.baptiste.bertron@gmail.com>
2025-07-22 11:05:10 +0200

    collapse

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import BannerAbout from "../../components/Banner/BannerAbout";
+import Collapse from "../../components/Collapse/Collapse";
 
 const About = () => {
   return (
     <div>
       <BannerAbout />
+      <Collapse title="ah">lol</Collapse>
     </div>
   );
 };
--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 import "./Collapse.scss";
+import { ReactComponent as Arrow } from "../../assets/logo/Vectorsvg.svg";
 
-function Collapse() {
-    const ({title, children}) = useState(0);
-  return ( 
-    <div>
-        const 
+function Collapse({ title, children }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="collapse">
+      <div className="collapse-header">
+        <h1>{title}</h1>
+        <Arrow
+          className={`arrow-collapse ${isOpen ? "open" : ""}`}
+          onClick={() => setIsOpen(!isOpen)}
+        />
+      </div>
+      {isOpen && <div className="content">{children}</div>}
     </div>
   );
 }
